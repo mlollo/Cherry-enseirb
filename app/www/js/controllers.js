91d9cfaed31fbe8b0ee5348b6ca57a752924b6f0
kaylan44 <guillaume.g44@hotmail.fr>
2017-04-04 16:32:47 +0200

    first test chore

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -24,7 +24,6 @@ angular.module('starter.controllers', [])
         $scope.primitives = data.url;
     })
     $scope.run_move = function(index) {
-        $scope.order = index;
         $http.get("http://localhost:8080/test/behave?name=" + $scope.primitives[index]);
     };
 
@@ -101,9 +100,9 @@ angular.module('starter.controllers', [])
 
 .controller('ChoregraphyCtrl', function($scope, $ionicPopup) {
     $scope.music = '/music/Magic_sys_FOU.mp3';
-    $scope.magicDisabled = false;
-    $scope.feelingsDisabled = false;
-    $scope.pitchDisabled = false;
+    $scope.music1Disabled = false;
+    $scope.music2Disabled = false;
+    $scope.music3Disabled = false;
 
     var audio = new Audio($scope.music);
 
@@ -120,29 +119,32 @@ angular.module('starter.controllers', [])
         audio.pause();
         audio = new Audio($scope.music);
         audio.play();
+
+        $http.get("http://localhost:8080/test/behave?name=" + $scope.droppedObjects1[0].primitive);
+
     };
 
-    $scope.setMagic = function() {
+    $scope.setMusic1 = function() {
         $scope.music='/music/Magic_sys_FOU.mp3';
-        $scope.magicDisabled = true;
-        $scope.feelingsDisabled = false;
-        $scope.pitchDisabled = false;
+        $scope.music1Disabled = true;
+        $scope.music2Disabled = false;
+        $scope.music3Disabled = false;
 
     };
 
-    $scope.setFeelings = function() {
+    $scope.setMusic2 = function() {
         $scope.music='/music/Steam_Phunk-Feelings.mp3';
-        $scope.feelingsDisabled = true;
-        $scope.magicDisabled = false;
-        $scope.pitchDisabled = false;
+        $scope.music2Disabled = true;
+        $scope.music1Disabled = false;
+        $scope.music3Disabled = false;
 
     };
 
-    $scope.setPitch = function() {
+    $scope.setMusic3 = function() {
         $scope.music='/music/Bad_pitched.mp3';
-        $scope.magicDisabled = false;
-        $scope.feelingsDisabled = false;
-        $scope.pitchDisabled = true;
+        $scope.music1Disabled = false;
+        $scope.music2Disabled = false;
+        $scope.music3Disabled = true;
     };
 
 
